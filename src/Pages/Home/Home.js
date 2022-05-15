ef083078ff187887ab0f4997802da7366d5833bc
telkomdev-udin <bang.kho48@gmail.com>
2022-05-15 22:16:06 +0700

    card setting

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -9,41 +9,22 @@ const Home = () => {
       </div>
       <div className="container">
         <div className="row">
-          <div className="col">
-            <img src={Image1} class="img-thumbnail" alt="..." />
-          </div>
-          <div className="col">
-            <img src={Image1} class="img-thumbnail" alt="..." />
-          </div>
-          <div className="col">
-            <img src={Image1} class="img-thumbnail" alt="..." />
-          </div>
-        </div>
-      </div>
-      <div className="container">
-        <div className="row">
-          <div className="col">
-            <img src={Image1} class="img-thumbnail" alt="..." />
-          </div>
-          <div className="col">
-            <img src={Image1} class="img-thumbnail" alt="..." />
-          </div>
-          <div className="col">
-            <img src={Image1} class="img-thumbnail" alt="..." />
-          </div>
-        </div>
-      </div>
-      <div className="container">
-        <div className="row">
-          <div className="col">
-            <img src={Image1} class="img-thumbnail" alt="..." />
-          </div>
-          <div className="col">
-            <img src={Image1} class="img-thumbnail" alt="..." />
-          </div>
-          <div className="col">
-            <img src={Image1} class="img-thumbnail" alt="..." />
-          </div>
+          {[1, 2, 3, 4, 5, 6, 7, 8, 9].map(() => {
+            return (
+              <div
+                className="position-relative col-4 mt-3 mx-auto"
+                style={{ width: "auto" }}
+              >
+                <img src={Image1} class="img-thumbnail rounded" alt="..." />
+                <h4
+                  className="position-absolute bottom-0 ms-4 mb-4"
+                  style={{ color: "white" }}
+                >
+                  Macaroni el salvador
+                </h4>
+              </div>
+            );
+          })}
         </div>
       </div>
     </div>
