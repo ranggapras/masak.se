43ef7dc7bc9282b830021b10b5b37d704540faa1
BangKho <bang.kho48@gmail.com>
2022-05-20 02:18:06 +0700

    Navbar fixing

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,9 +1,11 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useRef } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import { FaBars } from "react-icons/fa";
+import { BiLogOut } from "react-icons/bi";
 import { AiOutlineClose } from "react-icons/ai";
 import { useState } from "react";
 import { NavbarData } from "./NavbarData";
+import { Cookies } from "react-cookie";
 import { IconContext } from "react-icons";
 import Logo from "../../Assets/logo.svg";
 import SearchIcon from "../../Assets/search.svg";
@@ -12,6 +14,19 @@ import "./style.css";
 
 const Navbar = () => {
   const [sidebar, setSidebar] = useState(false);
+  const refSearch = useRef();
+  const cookie = new Cookies();
+  const navigate = useNavigate();
+
+  const search = () => {
+    console.log(refSearch.current.value);
+  };
+
+  const logout = () => {
+    cookie.remove("isLogin");
+    cookie.remove("idUser");
+    navigate("/login");
+  };
 
   const showSidebar = () => setSidebar(!sidebar);
 
@@ -25,17 +40,20 @@ const Navbar = () => {
         </div>
         <nav className={sidebar ? "nav-menu active" : "nav-menu"}>
           <ul className="nav-menu-items" onClick={showSidebar}>
-            <Link to="#" className="menu-bars">
-              <AiOutlineClose />
-            </Link>
             <li>
-              {NavbarData.map((d, i) => {
-                return (
-                  <li key={i} className={d.cName}>
-                    <Link to={d.path}>{d.icon}</Link>
-                  </li>
-                );
-              })}
+              <Link to="#" className="menu-bars">
+                <AiOutlineClose />
+              </Link>
+            </li>
+            {NavbarData.map((d, i) => {
+              return (
+                <li key={i} className={d.cName}>
+                  <Link to={d.path}>{d.icon}</Link>
+                </li>
+              );
+            })}
+            <li className="nav-text" onClick={() => logout()}>
+              <BiLogOut size={55} />
             </li>
           </ul>
         </nav>
@@ -47,14 +65,26 @@ const Navbar = () => {
           alt="logo"
         />
       </div>
-      <div style={{ border: ".2rem solid #FFAD61", borderRadius: "2rem" }}>
+      <div
+        style={{
+          border: ".2rem solid #FFAD61",
+          borderRadius: "2rem",
+          padding: "0 1rem",
+        }}
+      >
         <input
           style={{
             width: "30rem",
             border: "none",
           }}
+          ref={refSearch}
         ></input>
-        <img src={SearchIcon} />
+        <img
+          onClick={() => search()}
+          alt="..."
+          src={SearchIcon}
+          style={{ width: "1.5rem", margin: ".5rem 0", cursor: "pointer" }}
+        />
       </div>
     </div>
   );
